449250b166645a5e517ca81611e787ae1126f95d
Tyler van Leeuwen <tyler.vanleeuwen@students.mq.edu.au>
2025-08-08 14:45:00 +1000

    Names.java added

---
+++ src/Names.java
@@ -0,0 +1,15 @@
+public class Names {
+	public static void main(String[] Args) {
+		System.out.println("This group consists of:");
+		// Put your signature function on the below line!
+        System.out.println("Tyler van Leeuwen");
+        System.out.println("8===D");
+		PrintHarrisSig();
+		// Put your signature function on the above line!
+	}
+	
+	public static void PrintHarrisSig() {
+		System.out.println(".--.     /\\                ____");
+		System.out.println("'--'    /__\\    (^._.^)~ <(o.o )>");
+	}
+}
