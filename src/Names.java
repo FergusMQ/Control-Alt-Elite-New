3bc495438b8a1337e0a0ce3314563d1cbd898203
Tyler van Leeuwen <tyler.vanleeuwen@students.mq.edu.au>
2025-08-08 14:53:53 +1000

    Names tyler 2nd commit

--- src/Names.java
+++ src/Names.java
@@ -2,15 +2,15 @@ public class Names {
 	public static void main(String[] Args) {
 		System.out.println("This group consists of:");
 		// Put your signature function on the below line!
-        System.out.println("Tyler van Leeuwen");
-        System.out.println("8===D");
-		PrintHarrisSig();
+
+		PrintTylerSig();
+		PrintFergusSig();
 		// Put your signature function on the above line!
 	}
 	
-	public static void PrintHarrisSig() {
-		System.out.println(".--.     /\\                ____");
-		System.out.println("'--'    /__\\    (^._.^)~ <(o.o )>");
+	public static void PrintTylerSig() {
+        System.out.println("Tyler van Leeuwen");
+        System.out.println("8===D");
 	}
 	
 	public static void PrintFergusSig() {
